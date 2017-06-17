5c5262a41a952b881136f18bcd6e3fd5c41f5875
Alex <alex@alexjwebb.com>
2017-06-17 10:57:24 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+class HelloWorld {
+  constructor() {
+    console.log('running');
+  }
+}
+
+var helloWorld =  new HelloWorld;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        include: path.join(__dirname, 'src')
+      },
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    hot: true,
+    contentBase: path.join(__dirname, "dist"),
+    port: 8080
+  }
+};
